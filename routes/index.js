602c9c0724b38454fd6cb1e8b533e5f33ee13390
pokotyan <tomohiro_soga@yahoo.co.jp>
2017-05-25 15:25:13 +0900

    トップ画面にログインへのリンクを作成

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', user: req.user }); //github認証成功時はreq.userが存在する。githubのアカウントの情報が入ってる
 });
 
 module.exports = router;
