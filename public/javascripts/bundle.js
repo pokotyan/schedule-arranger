6db8342a66f5ef2a9d179966984913990b93ed60
pokotyan <tomohiro_soga@yahoo.co.jp>
2017-05-26 19:43:25 +0900

    バグ修正とコメントの追加
    schedule.jsが一部間違ってたせいで、正しいデータをビューに渡せてなかったので修正

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -10341,9 +10341,9 @@ $('.availability-toggle-button').each((i, e) => {
     const userId = button.data('user-id');
     const candidateId = button.data('candidate-id');
     const availability = parseInt(button.data('availability'));
-    const nextAvailability = (availability + 1) % 3;
+    const nextAvailability = (availability + 1) % 3;  //0 → 1 → 2 → 0 → 1 → 2 と循環させたいため、ここでは 1 を足して 3 の剰余を次の出欠の数値としています。
     $.post(`/schedules/${scheduleId}/users/${userId}/candidates/${candidateId}`,
-      { availability: nextAvailability },
+      { availability: nextAvailability },             //webapiに渡す引数
       (data) => {
         button.data('availability', data.availability);
         const availabilityLabels = ['欠', '？', '出'];
