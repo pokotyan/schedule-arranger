cb4012f5fcae466bad4ce112cfa212f7da507df7
pokotyan <tomohiro_soga@yahoo.co.jp>
2017-05-28 19:46:18 +0900

    ログインが必要なページにログインせずに訪問した際に、ログイン後元のページへリダイレクトする機能を実装
    １）authentication-ensurer.js
    ログインが必要なページへのアクセスは「authentication-ensurer」をかましている。
    ログインしているなら通して、ログインしていないならログインページへ飛ばすという処理。
    この「ログインしていないならログインページへ飛ばす」という処理にクエリを付与するよう修正。
    開こうとしていたページのurlをfromクエリとして付与し、ログインページへリダイレクトさせる。
    
    2）login.js
    で、ログインページのgetのアクションではfromクエリが存在すれば、それをloginFromクッキーとして保存する。
    そしてログインページが表示される。
    
    ３）app.js
    で、ログインページからgithubやfacebookの認証を行って認証が成功した際、
    loginFromクッキーが存在すれば、ルートページへリダイレクトさせるのではなく、loginFromクッキーに保存しておいたurlへリダイレクトさせる。

--- routes/authentication-ensurer.js
+++ routes/authentication-ensurer.js
@@ -1,8 +1,11 @@
 'use strict';
 
+//認証が必要なページの関所。認証してないならログインページにリダイレクトさせる
 function ensure(req, res, next){
+  //認証済みならページを表示
   if(req.isAuthenticated()){ return next(); }
-  res.redirect('/login');
+  //認証してないならログインページに飛ばす。また、どのページを開こうとしてたのかをfromクエリに含める
+  res.redirect('/login?from=' + req.originalUrl);
 }
 
 module.exports = ensure;
--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req, res, next)=>{
-  res.render('login', { user: req.user });
+  const from = req.query.from;   //ログインページへのgetリクエストが来たら、まずfromクエリを取得
+  if(from){                      //fromクエリがあれば、（ログインが必要なページをログインせずに開こうとした場合fromクエリがある。authentication-ensurer.jsを参照）
+    res.cookie('loginFrom', from, { expires: new Date(Date.now() + 600000) });  //そのfromクエリ(fromのurl)をloginFromクッキーとして保存。保存期間は 10 分
+  }
+  res.render('login');
 });
 
 module.exports = router;
